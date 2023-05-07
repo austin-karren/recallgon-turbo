7f9b49c24981cbab27aad26eb3526955def0c683
Austin Karren <austin.t.karren@gmail.com>
2023-05-07 10:32:52 -0600

    chore update packages, add renovate.json

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,2 +1,18 @@
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
+
+import { type AppRouter } from "./src/root";
+
 export { appRouter, type AppRouter } from "./src/root";
 export { createContext, type Context } from "./src/context";
+
+/**
+ * Inference helpers for input types
+ * @example type HelloInput = RouterInputs['example']['hello']
+ **/
+export type RouterInputs = inferRouterInputs<AppRouter>;
+
+/**
+ * Inference helpers for output types
+ * @example type HelloOutput = RouterOutputs['example']['hello']
+ **/
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
